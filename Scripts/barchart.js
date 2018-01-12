5dc1617f9a6e5d3449dabd91b2701b13d0fb7a21
Rosabrakkee <rosabrakkee@gmail.com>
2018-01-12 11:32:11 +0100

    average rainfall graph

--- Scripts/barchart.js
+++ Scripts/barchart.js
@@ -2,23 +2,40 @@
  * Rosa Brakkeee
  * DataProc Week 6
  *
- * This progam creates a barchart of the average income per provincie in NL
+ * This progam creates a barchart of the average income per month in NL
  *
  * Barchart was inspired by: http://bl.ocks.org/Jverma/887877fc5c2c2d99be10
  **/
+
+
 function makeGraph(data) {
   console.log("THE GRAPH")
+  var  data = [
+     {"month":"January","rainfall":"170.0"},
+     {"month":"Februari","rainfall":"130"},
+     {"month":"Maart","rainfall":"120"},
+     {"month":"April","rainfall":"80"},
+     {"month":"Mei","rainfall":"100"},
+     {"month":"Juni","rainfall":"70"},
+     {"month":"Juli","rainfall":"60"},
+     {"month":"August","rainfall":"60"},
+     {"month":"September","rainfall":"60"},
+     {"month":"Oktober","rainfall":"70"},
+     {"month":"November","rainfall":"120"},
+     {"month":"December","rainfall":"150"}
+   ]
+   console.log(data)
 
 
     // create canvas sizes
     var padding = {
-            top: 100,
-            right: 20,
-            bottom: 100,
-            left: 40
+          top: 100,
+          right: 20,
+          bottom: 100,
+          left: 40
         },
         width = 600 - padding.left - padding.right,
-        height = 600 - padding.top - padding.bottom;
+        height = 400 - padding.top - padding.bottom;
 
     // set the ranges of the axis
     var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
@@ -49,23 +66,23 @@ function makeGraph(data) {
         .attr("class", "d3-tip")
         .offset([-8, 0])
         .html(function(d) {
-            return Math.round(d.inkomen * 100) / 100 + "<strong>k</strong>"
+            return Math.round(d.rainfall) + "<strong>mm</strong>"
         });
 
     //svg for loading the tooltip
     svg2.call(tip)
 
     data.forEach(function(d) {
-        d.provincie = d.provincie;
-        d.inkomen = d.inkomen;
+        d.month = d.month;
+        d.rainfall = d.rainfall;
     });
 
     // scale the axis's to the loaded data
     x.domain(data.map(function(d) {
-        return d.provincie;
+        return d.month;
     }));
     y.domain([0, d3.max(data, function(d) {
-        return d.inkomen;
+       return parseInt(d.rainfall)
     })]);
 
     // add title to the graph
@@ -73,10 +90,10 @@ function makeGraph(data) {
         .attr("x", (width / 2))
         .attr("y", 0 - (padding.top / 3))
         .attr("text-anchor", "middle")
-        .style("font-size", "25px")
-        .text("Inkomen per provincie");
+        .style("font-size", "18px")
+        .text("Average rainfall Santa Cruz, Bolivia");
 
-    // add Xaxis with provincies rotated
+    // add Xaxis with months rotated
     svg2.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
@@ -96,25 +113,25 @@ function makeGraph(data) {
         .attr("y", -35)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Inkomen (k)");
+        .text("rainfall (mm)");
 
     // Add the bars to the chart and add the mouse hover element
     svg2.selectAll("barchart")
         .data(data)
         .enter().append("rect")
         .attr("id", function(d) {
-            return d.provincie
+            return d.month
         })
         .attr("class", "barchart")
         .attr("x", function(d) {
-            return x(d.provincie);
+            return x(d.month);
         })
         .attr("width", x.rangeBand())
         .attr("y", function(d) {
-            return y(d.inkomen);
+            return y(d.rainfall);
         })
         .attr("height", function(d) {
-            return height - y(d.inkomen);
+            return height - y(d.rainfall);
         })
         .on("mouseover", tip.show)
         .on("mouseout", tip.hide)
--- ToDo.md
+++ ToDo.md
@@ -1,6 +1,7 @@
 ## to do list
 - get route on map
 - find data rainfall/ temperatures (API)
+- RN: API weahter forecast 5 days
 - find data valuta
 - bootstrap interface
 http://jsbin.com/gezij/1/edit?html,output
